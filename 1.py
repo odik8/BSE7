4b71f153e030c4c672379e161da4d496c00332bb
odik8 <107572344+odik8@users.noreply.github.com>
2023-12-26 18:06:39 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 def main():
-    A = [int(input(f"Введите {i + 1}-й элемент списка: ")) for i in range(10)]
+    user_input = list(map(float, input("Введите числа через пробел: ").split()))
 
     sum_negative_multiples_of_7 = 0
     count_negative_multiples_of_7 = 0
 
-    for num in A:
+    for num in user_input:
         if num < 0 and num % 7 == 0:
             sum_negative_multiples_of_7 += num
             count_negative_multiples_of_7 += 1
@@ -16,12 +16,13 @@ def main():
     print(f"Сумма отрицательных элементов, кратных 7: {sum_negative_multiples_of_7}")
     print(f"Количество отрицательных элементов, кратных 7: {count_negative_multiples_of_7}")
 
-    sum_negative_multiples_of_7 = sum(num for num in A if num < 0 and num % 7 == 0)
-    count_negative_multiples_of_7 = sum(1 for num in A if num < 0 and num % 7 == 0)
+    sum_negative_multiples_of_7 = sum(num for num in user_input if num < 0 and num % 7 == 0)
+    count_negative_multiples_of_7 = sum(1 for num in user_input if num < 0 and num % 7 == 0)
 
     print("\nС использованием List Comprehensions:")
     print(f"Сумма отрицательных элементов, кратных 7: {sum_negative_multiples_of_7}")
     print(f"Количество отрицательных элементов, кратных 7: {count_negative_multiples_of_7}")
+
 
 if __name__ == "__main__":
     main()
